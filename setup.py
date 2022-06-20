b7ccceca55fb6bbbc8a6f029730ef6c0c37604c7
Z-tech2 <107017335+Z-tech2@users.noreply.github.com>
2022-06-20 14:44:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='MoreOperators',
+    version='0.1',
+    license='None',
+    author="Syed Hasan",
+    author_email='mathman5959@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Z-tech2/MoreOperators',
+    keywords='MoreOperators/Logic gates',
+    install_requires=[
+          ,
+      ],
+
+)
